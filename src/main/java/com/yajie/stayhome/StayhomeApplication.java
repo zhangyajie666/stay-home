d8b22c5103bd4dff4e99583ef8ac841daddf81e3
Yajie <Yajie@qq.com>
2020-03-28 11:16:58 +0800

    First Commit 新建项目

---
+++ src/main/java/com/yajie/stayhome/StayhomeApplication.java
@@ -0,0 +1,13 @@
+package com.yajie.stayhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yajie/stayhome/StayhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yajie.stayhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
